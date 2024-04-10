0bfd9a5cc0bb5b14b99eee5ae7540736a0663b0f
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-04-10 12:13:29 -0300

    feat: add controller e implementação dos serviços

---
+++ src/main/java/com/gabriel/gof/controller/ClienteController.java
@@ -0,0 +1,44 @@
+package com.gabriel.gof.controller;
+
+import com.gabriel.gof.model.Cliente;
+import com.gabriel.gof.service.ClienteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Cliente>> buscarTodos() {
+        return ResponseEntity.ok(clienteService.buscarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(clienteService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
+        clienteService.inserir(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        clienteService.atualizar(id, cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        clienteService.deletar(id);
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/java/com/gabriel/gof/service/ClienteService.java
+++ src/main/java/com/gabriel/gof/service/ClienteService.java
@@ -1,11 +1,14 @@
 package com.gabriel.gof.service;
 
 import com.gabriel.gof.model.Cliente;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface ClienteService {
     Iterable<Cliente> buscarTodos();
     Cliente buscarPorId(Long id);
     void inserir(Cliente cliente);
-    void atualizar(Cliente cliente);
+    void atualizar(Long id, Cliente cliente);
     void deletar(Long id);
 }
